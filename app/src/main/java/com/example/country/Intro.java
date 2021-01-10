6020d2feda9433be8fe45998a21308febfc74233
tommercoder <48603935+tommercoder@users.noreply.github.com>
2021-01-10 23:59:44 +0200

    intro done

--- app/src/main/java/com/example/country/Intro.java
+++ app/src/main/java/com/example/country/Intro.java
@@ -1,6 +1,5 @@
 package com.example.country;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -20,17 +19,29 @@ public class Intro extends AppIntro {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_intro);
 
+addSlide(AppIntroFragment.newInstance(
+        "Welcome to the Country", "Press skip or continue to learn how to use the app.",R.drawable.hello, ContextCompat.getColor(getApplicationContext(),R.color.stopgrad)
+));
 
-        addSlide(AppIntroFragment.newInstance(
-                "Welcome to the Country", "Here you can vote for anyone you want:)",R.drawable.hello, ContextCompat.getColor(getApplicationContext(),R.color.stopgrad)
-        ));
+ addSlide(AppIntroFragment.newInstance(
+         "Our Calendar", "Quick way to locate our recent events. You can also choose date to navigate to the event.",R.drawable.calendar_r, ContextCompat.getColor(getApplicationContext(),R.color.stopgrad)
+ ));
 
-        addSlide(AppIntroFragment.newInstance(
-                "Our Calendar", "Quick way to locate our recent events. You can also choose date to navigate to the event",R.drawable.calendar_r, ContextCompat.getColor(getApplicationContext(),R.color.stopgrad)
-        ));
+ addSlide(AppIntroFragment.newInstance(
+         "Navigation menu", "From here you can go everywhere you want.",R.drawable.menu_r, ContextCompat.getColor(getApplicationContext(),R.color.stopgrad)
+ ));
+ addSlide(AppIntroFragment.newInstance(
+         "Home section", "Choose and vote system. Voting has never been simpler.",R.drawable.election_r, ContextCompat.getColor(getApplicationContext(),R.color.stopgrad)
+ ));
+ addSlide(AppIntroFragment.newInstance(
+         "User section", "Change your profile or logout from application.",R.drawable.user_r, ContextCompat.getColor(getApplicationContext(),R.color.stopgrad)
+ ));
+ addSlide(AppIntroFragment.newInstance(
+         "Support section", "In case of any troubles, you can contact us any time.",R.drawable.support_r, ContextCompat.getColor(getApplicationContext(),R.color.stopgrad)
+ ));
 
-        showSkipButton(true);
-        setProgressBarVisibility(true);
+       showSkipButton(true);
+       setProgressBarVisibility(true);
 
 
 
